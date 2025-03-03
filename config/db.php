1c8da00128d83070799abc4a55255f2e72e8d138
xaos10123 <xaos10123@gmail.com>
2025-03-03 12:10:38 +0700

    Создание подключения к БД

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "registration_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Ошибка подключения: " . $conn->connect_error);
+}
